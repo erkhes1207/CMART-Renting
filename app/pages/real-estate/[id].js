7d88341936cfbdc2a429b04644520c627f767a8b
Chingun <97659944+solac911@users.noreply.github.com>
2022-10-24 13:06:36 +0800

    added 3 forms & profile

--- app/pages/real-estate/[id].js
+++ app/pages/real-estate/[id].js
@@ -3,45 +3,45 @@ import Layout from "../../components/Layout";
 const RealEstateDetail = () => {
   return (
     <Layout>
-      <div className='flex w-full h-full justify-center items-center'>
-        <div className='flex max-w-7xl w-full gap-5'>
-          <section className='overflow-hidden text-gray-700 w-full'>
-            <div className='mx-auto lg:px-32 w-full'>
-              <div className='flex w-full'>
-                <div className='w-1/2 p-1'>
+      <div className="flex w-full h-full justify-center items-center">
+        <div className="flex max-w-7xl w-full gap-5">
+          <section className="overflow-hidden text-gray-700 w-full">
+            <div className="mx-auto lg:px-32 w-full">
+              <div className="flex w-full">
+                <div className="w-1/2 p-1">
                   <img
-                    alt='gallery'
-                    className='block object-cover object-center rounded-tl-2xl rounded-bl-2xl w-full h-[335px]'
-                    src='https://a0.muscache.com/im/pictures/miso/Hosting-629170893088045571/original/b4542d4a-2487-47a8-9035-0ba2858213d8.jpeg?im_w=720'
+                    alt="gallery"
+                    className="block object-cover object-center rounded-tl-2xl rounded-bl-2xl w-full h-[335px]"
+                    src="https://a0.muscache.com/im/pictures/miso/Hosting-629170893088045571/original/b4542d4a-2487-47a8-9035-0ba2858213d8.jpeg?im_w=720"
                   />
                 </div>
-                <div className='flex flex-wrap w-1/2 h-full'>
-                  <div className='w-1/2 pr-1 py-1 h-44'>
+                <div className="flex flex-wrap w-1/2 h-full">
+                  <div className="w-1/2 pr-1 py-1 h-44">
                     <img
-                      alt='gallery'
-                      className='block object-cover object-center w-80 h-40'
-                      src='https://a0.muscache.com/im/pictures/miso/Hosting-629170893088045571/original/6b18c223-3b31-48ab-bd36-3073048825e4.jpeg?im_w=720'
+                      alt="gallery"
+                      className="block object-cover object-center w-80 h-40"
+                      src="https://a0.muscache.com/im/pictures/miso/Hosting-629170893088045571/original/6b18c223-3b31-48ab-bd36-3073048825e4.jpeg?im_w=720"
                     />
                   </div>
-                  <div className='w-1/2 py-1 h-44'>
+                  <div className="w-1/2 py-1 h-44">
                     <img
-                      alt='gallery'
-                      className='block object-cover object-center rounded-tr-2xl w-80 h-40'
-                      src='https://a0.muscache.com/im/pictures/miso/Hosting-629170893088045571/original/bff0e97e-dc0d-425f-890d-cedd31697ce7.jpeg?im_w=720'
+                      alt="gallery"
+                      className="block object-cover object-center rounded-tr-2xl w-80 h-40"
+                      src="https://a0.muscache.com/im/pictures/miso/Hosting-629170893088045571/original/bff0e97e-dc0d-425f-890d-cedd31697ce7.jpeg?im_w=720"
                     />
                   </div>
-                  <div className='w-1/2 pr-1 h-44'>
+                  <div className="w-1/2 pr-1 h-44">
                     <img
-                      alt='gallery'
-                      className='block object-cover object-center w-80 h-40'
-                      src='https://a0.muscache.com/im/pictures/miso/Hosting-629170893088045571/original/58cb528b-86aa-4d91-967c-e55cc6b6a245.jpeg?im_w=720'
+                      alt="gallery"
+                      className="block object-cover object-center w-80 h-40"
+                      src="https://a0.muscache.com/im/pictures/miso/Hosting-629170893088045571/original/58cb528b-86aa-4d91-967c-e55cc6b6a245.jpeg?im_w=720"
                     />
                   </div>
-                  <div className='w-1/2 h-44'>
+                  <div className="w-1/2 h-44">
                     <img
-                      alt='gallery'
-                      className='block object-cover object-center rounded-br-2xl w-80 h-40'
-                      src='https://a0.muscache.com/im/pictures/miso/Hosting-629170893088045571/original/3d7c334f-03b7-4fc6-9d95-a5221fbb4177.jpeg?im_w=720'
+                      alt="gallery"
+                      className="block object-cover object-center rounded-br-2xl w-80 h-40"
+                      src="https://a0.muscache.com/im/pictures/miso/Hosting-629170893088045571/original/3d7c334f-03b7-4fc6-9d95-a5221fbb4177.jpeg?im_w=720"
                     />
                   </div>
                 </div>
---
+++ app/pages/tenant-form.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from "react";
+import Layout from "../components/Layout";
+export default function TenantForm() {
+  const [state, setState] = useState([]);
+
+  function handleChange(e) {
+    setState({ ...state, [e.target.name]: e.target.value });
+  }
+
+  return (
+    <Layout>
+      <div className="w-full p-5 lg:p-3">
+        <div className="mt-10">
+          <div className="font-body">
+            <div className="w-full p-6 mx-auto my-5 border rounded-2xl space-y-6 sm:w-4/5 lg:w-1/2">
+              <div className="space-y-2">
+                <h3 className="text-xl font-semibold">Profile Information</h3>
+                <span className="text-gray-500">
+                  Fill out a few details to start receiving payments directly to
+                  your bank account
+                </span>
+              </div>
+              <div className="space-y-2">
+                <h3 className="uppercase text-sm font-semibold">full name</h3>
+                <input
+                  type="text"
+                  className="w-full h-12 px-3 border rounded-md focus:outline-none"
+                  placeholder="John Smith"
+                />
+              </div>
+              <div className="space-y-2">
+                <div className="flex">
+                  <h3 className="uppercase text-sm font-semibold">Phone</h3>
+                  <h3 className="uppercase text-sm font-semibold ml-[368px]">
+                    Email
+                  </h3>
+                </div>
+                <div className="flex">
+                  <input
+                    type="text"
+                    className="w-[365px] h-12 px-3 border rounded-md focus:outline-none"
+                    placeholder="99310720"
+                  />
+                  <input
+                    type="text"
+                    className="w-[365px] h-12 px-3 border rounded-md focus:outline-none ml-12"
+                    placeholder="chingunee.dev@gmail.com"
+                  />
+                </div>
+              </div>
+              <div className="space-y-2">
+                <div className="flex">
+                  <h3 className="uppercase text-sm font-semibold ">
+                    Id number
+                  </h3>
+                  <label
+                    htmlFor="genders"
+                    className="uppercase text-sm font-semibold ml-[338px]"
+                  >
+                    Gender
+                  </label>
+                </div>
+                <div className="flex">
+                  <input
+                    type="text"
+                    className="w-[368px] h-12 px-3 border rounded-md focus:outline-none"
+                    placeholder="SSH03271029"
+                  />
+                  <select
+                    name="genders"
+                    id="genders"
+                    className="w-[368px] h-12 px-3 border rounded-md focus: outline-none ml-11"
+                  >
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                  </select>
+                </div>
+              </div>
+              <div className="space-y-2">
+                <h3 className="uppercase text-sm font-semibold">Bio</h3>
+                <textarea
+                  className="border rounded-md pl-2 pt-2"
+                  name=""
+                  id=""
+                  cols="103"
+                  rows="10"
+                  placeholder="Write a short and sweet description about yourself..."
+                ></textarea>
+              </div>
+              <div className="flex justify-center">
+                <button className="w-1/5 flex justify-center items-center border rounded cursor-pointer p-2 text-white bg-[#f82f5d]">
+                  <span>Mint Tenant NFT</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
